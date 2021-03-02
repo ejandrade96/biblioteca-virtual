df862fe443fcd998b22fdf80cfc805b4873a7141
Elton Andrade <eltonfernandesandrade@hotmail.com>
2021-03-01 23:17:32 -0300

    Criando o projeto e as classes para os testes de unidade

---
+++ Tests/Unit/Services/Student.cs
@@ -0,0 +1,68 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Unit.Services
+{
+  public class Student
+  {
+    [Fact]
+    public async Task Deve_Cadastrar_Um_Aluno_Quando_Enviar_Dados_Certos()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Retornar_Erro_Quando_Tentar_Cadastrar_Um_Aluno_Com_Email_Existente()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Retornar_Erro_Quando_Tentar_Cadastrar_Um_Aluno_Com_Login_Existente()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Listar_Todos_Alunos()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Atualizar_Um_Aluno_Quando_Enviar_Dados_Certos()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Retornar_Erro_Quando_Tentar_Atualizar_Um_Aluno_Inexistente()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Retornar_Erro_Quando_Tentar_Atualizar_Um_Aluno_Com_Email_Existente()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Retornar_Erro_Quando_Tentar_Atualizar_Um_Aluno_Com_Login_Existente()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Deletar_Um_Aluno()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Retornar_Erro_Quando_Tentar_Deletar_Um_Aluno_Inexistente()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Retornar_Um_Aluno_Por_Id()
+    {
+    }
+
+    [Fact]
+    public async Task Deve_Retornar_Erro_Quando_Tentar_Buscar_Um_Aluno_Inexistente_Por_Id()
+    {
+    }
+  }
+}
